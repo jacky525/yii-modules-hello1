a7ecb03923e5fe19a462c8c5494fa578c530987a
jacky.lin[林揚正] <jacky.lin@jackylin.local>
2019-05-22 15:48:16 +0800

    jackey

--- views/default/index.php
+++ views/default/index.php
@@ -11,6 +11,7 @@ $this->breadcrumbs=array(
 This is the view content for action "<?php echo $this->action->id; ?>".
 The action belongs to the controller "<?php echo get_class($this); ?>"
 in the "<?php echo $this->module->id; ?>" module.
+     test test version 1 test
 </p>
 <p>
 You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
